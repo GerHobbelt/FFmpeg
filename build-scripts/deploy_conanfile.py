8612358929aa3593d6e96fd97d5b12713a6e3251
joshyoo-topazlabs <josh.yoo@topazlabs.com>
2025-07-25 11:58:30 -0500

    update videoai for rtx

--- build-scripts/deploy_conanfile.py
+++ build-scripts/deploy_conanfile.py
@@ -13,7 +13,7 @@ class conanRecipe(ConanFile):
             self.options["openssl"].shared = False
 
     def requirements(self):
-        self.requires("videoai/[~1.9.0]")
+        self.requires("videoai/1.9.38-rtx")
         if self.settings.os == "Macos" and self.settings.arch == "x86_64":
             self.requires("zimg/3.0.5@josh/oiio3")
         else:
