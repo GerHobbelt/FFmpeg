42bc48a8f8ef0c0d7725a0521cdbd0b63e62c1a3
joshyoo-topazlabs <josh.yoo@topazlabs.com>
2025-09-18 10:36:24 -0500

    update video for tensorrt enterprise 10.11

--- build-scripts/deploy_conanfile.py
+++ build-scripts/deploy_conanfile.py
@@ -14,7 +14,7 @@ class conanRecipe(ConanFile):
             self.options["openssl"].shared = False
 
     def requirements(self):
-        self.requires("videoai/[~2.0.0]")
+        self.requires("videoai/1.9.51-tensorrt1011")
         if self.settings.os == "Macos" and self.settings.arch == "x86_64":
             self.requires("zimg/3.0.5@josh/oiio3")
         else:
