ea686518fc056337cb13f3d0d27aeb798cdee0a1
joshyoo-topazlabs <josh.yoo@topazlabs.com>
2025-11-05 16:30:33 -0600

    static aom

--- build-scripts/deploy_conanfile.py
+++ build-scripts/deploy_conanfile.py
@@ -14,7 +14,7 @@ class conanRecipe(ConanFile):
             self.options["openssl"].shared = False
 
         if self.settings.os == "Windows" and self.settings.arch == "x86_64":
-            self.options["libaom-av1"].shared = True
+            self.options["libaom-av1"].shared = False
 
     def requirements(self):
         self.requires("videoai/[~2.0.0]")
